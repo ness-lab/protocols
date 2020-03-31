622c0667a2f305535fee8530c3aa3ce9904c85bf
James Santangelo <james.santangelo37@gmail.com>
2020-03-30 20:45:35 -0400

    Updating word output

--- libraryPrep_Illumina/create_pdf.sh
+++
@@ -1,5 +0,0 @@
-pandoc libraryPrep_protocol.md \
--o libraryPrep_protocol.pdf \
---latex-engine=xelatex \
--V mainfont:"Times New Roman" \
---variable urlcolor=cyan
--- libraryPrep_Illumina/create_word.sh
+++ libraryPrep_Illumina/create_word.sh
@@ -1,5 +1,6 @@
 pandoc libraryPrep_protocol.md \
 -o libraryPrep_protocol.docx \
---latex-engine=xelatex \
--V mainfont:"Times New Roman" \
---variable urlcolor=cyan
+-F pandoc-crossref \
+-F pandoc-citeproc \
+--pdf-engine=xelatex \
+-V mainfont:"Times New Roman"
